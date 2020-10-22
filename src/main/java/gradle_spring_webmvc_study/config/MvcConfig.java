e1995b015b583ba635c6ceb620448a8e1d7e5ce5
teacher <net94@naver.com>
2020-10-22 12:09:20 +0900

    Bean Validation 기능 추가

--- src/main/java/gradle_spring_webmvc_study/config/MvcConfig.java
+++ src/main/java/gradle_spring_webmvc_study/config/MvcConfig.java
@@ -1,6 +1,9 @@
 package gradle_spring_webmvc_study.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -26,6 +29,26 @@ public class MvcConfig implements WebMvcConfigurer{
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/main").setViewName("main");
     }
+    
+    /*다국어 설정*/
+    @Bean
+    public MessageSource messageSource() { // 빈의 아이디를 반드시 “messageSource”로 지정해야 됨
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setBasename("message.label");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
+    }
+    
+    /**
+     *  객체를 글로벌 범위 Validator로 사용
+     *  글로벌 범위 Validator를 지정하면 
+     *  @Valid 애노테이션을 사용해서 Validator를 적용
+     */
+//    @Override
+//    public Validator getValidator() {
+//        return new RegisterRequestValidator();
+//    }
+
 }
 
 
--- src/main/java/gradle_spring_webmvc_study/controller/RegisterController.java
+++ src/main/java/gradle_spring_webmvc_study/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package gradle_spring_webmvc_study.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,7 +54,11 @@ public class RegisterController {
     }
 
     @PostMapping("/step3")
-    public String handleStep3(RegisterRequest regReq) {
+    public String handleStep3(@Valid RegisterRequest regReq, Errors errors) {
+//        new RegisterRequestValidator().validate(regReq, errors);
+        if (errors.hasErrors())
+            return "register/step2";
+
         try {
             memberRegisterService.regist(regReq);
             return "register/step3";
